63250d2d01394898ca001bd0a278ee8a77a3e561
shimada337 <eisukeqoo@icloud.com>
2022-04-07 01:34:03 +0000

    クラス継承

---
+++ sub_class.rb
@@ -0,0 +1,28 @@
+# クラス継承
+
+class User
+  def initialize(name)
+    @name = name
+  end
+  
+  def hello
+    puts "Hello! I am #{@name}."
+  end
+end
+
+class AdminUser < User
+  def admin_hello
+    puts "Hello! I am #{@name} from AdminUser."
+  end
+  
+  def hello
+    puts 'Admin!'
+  end
+end
+
+# nakamura = User.new('Nakamura')
+# nakamura.hello
+
+sato = AdminUser.new('Sato')
+sato.hello
+sato.admin_hello
