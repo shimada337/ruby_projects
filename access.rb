865e4fad5c776bb1f721cd47100909244a6ae919
shimada337 <eisukeqoo@icloud.com>
2022-04-08 00:49:00 +0000

    メソッド公開範囲

---
+++ access.rb
@@ -0,0 +1,19 @@
+class User
+  def initialize(name)
+    @name = name
+  end
+  
+  def say
+    hello
+  end
+  
+  private
+  
+    def hello
+     puts "Hello! I am #{@name}."
+    end
+end
+
+nakamura = User.new('nakamura')
+# nakamura.hello
+nakamura.say
